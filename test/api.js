7b280b62069a5ed839efe2fba33a8754b98fde2d
Travis Clarke <travis.m.clarke@gmail.com>
2017-07-19 19:55:25 -0700

    update tests and add TODO

--- test/api.js
+++ test/api.js
@@ -2,7 +2,15 @@
 
 var hli = require('../');
 var Indexer = new hli();
-Indexer.setInput('content/**');
-Indexer.setOutput('public/lunr.json');
+
+// YAML
+Indexer.setInput('content/test-yaml.md');
+Indexer.setOutput('public/lunr-api-yaml.json');
+Indexer.setLanguage('yaml');
+Indexer.index();
+
+// TOML
+Indexer.setInput('content/test-toml.md');
+Indexer.setOutput('public/lunr-api-toml.json');
 Indexer.setLanguage('toml');
 Indexer.index();
